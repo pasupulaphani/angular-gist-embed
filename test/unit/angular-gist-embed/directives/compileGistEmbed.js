6757c4d220de9b97d2bf5ebe6db33949a0bbe04b
pasupulaphani <pasupulaphani@gmail.com>
2015-01-18 03:03:30 +0000

    setting up deployment

--- test/unit/angular-gist-embed/directives/compileGistEmbed.js
+++ test/unit/angular-gist-embed/directives/compileGistEmbed.js
@@ -4,21 +4,23 @@ describe("compileGistEmbed", function() {
 
     beforeEach(module("gist-embed"));
 
-    var service,
+    var $sce,
         $compile,
         $rootScope;
 
-    beforeEach(inject(function(_$compile_, _$rootScope_, gistEmbed) {
+    beforeEach(inject(function(_$compile_, _$rootScope_, _$sce_) {
+        jasmine.clock().install();
+
         $compile = _$compile_;
         $rootScope = _$rootScope_;
-        service = gistEmbed;
-        service.init();
+        $sce = _$sce_;
     }));
+
 
     it("should substitute code in binded html", function() {
 
         // set up data
-        $rootScope.html = "<code data-gist-id='9463004' data-gist-file='mocha_tests.js'></code>";
+        $rootScope.html = $sce.trustAsHtml("<code data-gist-id='9463004' data-gist-file='mocha_tests.js'></code>");
 
         // Compile a piece of HTML containing the directive
         var element = $compile("<div compile-gist-embed ng-bind-html='html'></div>")($rootScope);
@@ -26,8 +28,11 @@ describe("compileGistEmbed", function() {
         // fire all the watches, so the scope expression {{1 + 1}} will be evaluated
         $rootScope.$apply();
 
+        setTimeout(function() {
+        }, 300);
+        jasmine.clock().tick(301);
+
         // Check that the compiled element contains the templated content
-        console.error(element.children())
         expect(element.children().length).toBeGreaterThan(0);
     });
 });
