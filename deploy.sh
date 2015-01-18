4bde1d62e4afae9417794dc4a148b518522c42d6
pasupulaphani <pasupulaphani@gmail.com>
2015-01-18 00:42:18 +0000

    added jquery dependency

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,23 @@
 # bash
 
-echo "############### START: GRUNT #################"
+echo "############### START: GRUNT #################\n"
 grunt -v
-echo "############### END: GRUNT #################"
+echo "############### END: GRUNT #################\n"
 
 git add .
 git add -u
 
-echo "############### FILE CHANGES #################"
+echo "############### FILE CHANGES #################\n"
 git status
 
-echo "############### START: COMMIT and PUSH #################"
+echo "############### START: COMMIT and PUSH #################\n"
 git commit -m "setting up deployment"
 git push origin master
-echo "############### END: COMMIT and PUSH #################"
+echo "############### END: COMMIT and PUSH #################\n"
 
-version=cat bower.json | underscore select .version
-echo "############### BOWER RELEASE: $version #################"
-# git tag -a v0.0.2 -m "Release version 0.0.2"
+version=`cat ./bower.json | underscore select .version --outfmt text`
+
+echo "############### START BOWER RELEASE: $version #################\n"
+git tag -f -a v$version -m "Release version $version"
+git push --tags
+echo "############### END BOWER RELEASE: $version #################\n"
