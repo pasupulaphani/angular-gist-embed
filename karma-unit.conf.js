f6c6023e3a6715c81e3127376bedab0bbbfe32c3
pasupulaphani <pasupulaphani@gmail.com>
2015-01-18 00:54:03 +0000

    setting up deployment

--- karma-unit.conf.js
+++ karma-unit.conf.js
@@ -17,6 +17,7 @@ module.exports = function(config) {
         // list of files / patterns to load in the browser
         files: [
             'bower_components/angular/angular.js',
+            'bower_components/jquery/jquery.js',
             'bower_components/angular-mocks/angular-mocks.js',
             'dist/angular-gist-embed.js',
             'test/unit/**/*.js'
