8b0538bbc8a8209a9c5f161bda6170b13e81e8b6
Phaninder <pasupulaphani@gmail.com>
2016-06-26 12:17:47 +0100

    setting up deployment

--- dist/angular-gist-embed.js
+++ dist/angular-gist-embed.js
@@ -45,7 +45,7 @@ angular.module('gist-embed.services')
     .service(
         'gistEmbed', ['$document', '$q', '$timeout', function($document, $q, $timeout) {
 
-            var libUrl = '//cdnjs.cloudflare.com/ajax/libs/gist-embed/2.0/gist-embed.min.js';
+            var libUrl = '//cdnjs.cloudflare.com/ajax/libs/gist-embed/2.2/gist-embed.min.js';
 
             var loadSdkAsync = function(src) {
                 var deferred = $q.defer();
@@ -89,6 +89,34 @@ angular.module('gist-embed.services')
                     }, 300);
                 })
                 .then(function() {
+                
+                	// register trigger gist on every route change
+                	$rootScope.$on('$viewContentLoaded',function(){
+                		
+                		// as ng-view can be used as class, attribute and tag, this accounts for all cases
+                		// ng-view is selected to ensure gist is run only on the newly added template
+                		var runGist = function(routeTemplates){
+                			angular.forEach(routeTemplates,function(template){
+                				if(template.length > 0){
+                					template.find('[data-gist-id]').gist();
+                				}
+                			});
+                		},
+                		routeTemplates = [];
+                		routeTemplates.push( angular.element('ng-view') );
+                		routeTemplates.push( angular.element('[ng-view]') );
+                		routeTemplates.push( angular.element('.ng-view') );
+                		
+                		// initialize gist on new elements
+                        angular.element(document).ready(function() {
+		
+                            if (typeof(angular.element(document).gist) === 'function') {
+                                runGist(routeTemplates);
+                            }
+                          
+                        });
+                        
+                	});
 
                     // register trigger gist on every template include
                     $rootScope.$on('$includeContentLoaded', function() {
